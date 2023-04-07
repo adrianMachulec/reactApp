fa8a19435fb2be1fff0f17356bdb83590ee61952
Adrian Machulec <adriandamianmachulec@gmail.com>
2023-04-07 19:21:29 +0200

    First components

---
+++ src/components/Hotels/Hotels.js
@@ -0,0 +1,16 @@
+import {Component} from "react"
+import Hotel from "./Hotel/Hotel"
+
+class Hotels extends Component {
+    render(){
+        return(
+            <div>
+                Hotels
+                <Hotel />
+                <Hotel />
+            </div>
+        )
+    }
+}
+
+export default Hotels
