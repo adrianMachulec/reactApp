214e7cc648b8d7cbb4c38c2bce79cf2cbdf22c1c
Adrian Machulec <adriandamianmachulec@gmail.com>
2023-04-09 20:56:58 +0200

    Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Component } from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import Menu from "./components/Menu/Menu";
 import Hotels from "./components/Hotels/Hotels";
 import LoadingIcon from "./components/UI/LoadingIcon/LoadingIcon";
+import Searchbar from "./components/UI/Searchbar/Searchbar";
+import Layout from "./components/Layout/Layout";
 
 class App extends Component {
   hotels = [
@@ -25,34 +28,44 @@ class App extends Component {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vestibulum, risus vitae venenatis pulvinar, neque velit laoreet quam, a blandit enim dolor ac ligula.",
       image: "",
     },
-  ]
+  ];
 
   state = {
     hotels: [],
-    loading: true
+    loading: true,
   };
 
   searchHandler(term) {
-    const hotels = [...this.hotels].filter(x => x.name.toLowerCase().includes(term.toLowerCase()))
-    this.setState({hotels})
+    const hotels = [...this.hotels].filter((x) =>
+      x.name.toLowerCase().includes(term.toLowerCase())
+    );
+    this.setState({ hotels });
   }
 
   componentDidMount() {
-    setTimeout(()=>{
-      this.setState({hotels: this.hotels, loading: false})
-    }, 1000)
+    setTimeout(() => {
+      this.setState({ hotels: this.hotels, loading: false });
+    }, 1000);
   }
 
   render() {
     return (
-      <div className="App">
-        <Header onSearch={(term) => this.searchHandler(term)} />
-        <Menu />
-        {this.state.loading 
-          ? <LoadingIcon /> 
-          : <Hotels hotels={this.state.hotels} />
+      <Layout
+        header={
+          <Header onSearch={(term) => this.searchHandler(term)}>
+            <Searchbar onSearch={(term) => this.searchHandler(term)} />
+          </Header>
         }
-      </div>
+        menu={<Menu />}
+        content={
+          this.state.loading ? (
+            <LoadingIcon />
+          ) : (
+            <Hotels hotels={this.state.hotels} />
+          )
+        }
+        footer={<Footer />}
+      />
     );
   }
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <div className="text-center mt-3">
+            Noclegi 2023
+        </div>
+    )
+}
