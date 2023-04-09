2431eb0cd57a6a259d3aab77804d324d38d799b9
Adrian Machulec <adriandamianmachulec@gmail.com>
2023-04-09 14:44:07 +0200

    Searchbar search function

--- src/components/Header/Searchbar/Searchbar.js
+++ src/components/Header/Searchbar/Searchbar.js
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 function Searchbar() {
+
+  const [term, setTerm] = useState('')
+
+  const search = () => {
+    console.log('Szukaj', term)
+  }
+
+  const onKeyDownHandler = (e) => {
+    if(e.key === 'Enter') search()
+  }
+
   return (
     <div className="d-flex">
       <input
+        value={term}
+        onKeyDown={onKeyDownHandler}
+        onChange={e => setTerm(e.target.value) }
         className="form-control"
         type="text"
         placeholder="Szukaj..."
       />
-      <button className="btn btn-secondary ms-1">Szukaj</button>
+      <button 
+        className="btn btn-secondary ms-1"
+        onClick={search}
+      >Szukaj</button>
     </div>
   );
 }
