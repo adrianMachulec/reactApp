550686aa7f30b7523463ea42aabf81c447ed3487
Adrian Machulec <adriandamianmachulec@gmail.com>
2023-04-11 23:12:47 +0200

    useMemo

--- src/components/Hotels/Hotels.js
+++ src/components/Hotels/Hotels.js
@@ -1,19 +1,25 @@
-import { PureComponent } from "react";
 import Hotel from "./Hotel/Hotel";
 import styles from "./Hotels.module.css";
 import PropTypes from "prop-types";
+import React, { useMemo } from "react";
 
-class Hotels extends PureComponent {
-  render() {
-    return (
-      <div className={styles.container}>
-        <h2 className={styles.title}>Oferty:</h2>
-        {this.props.hotels.map((hotel) => (
-          <Hotel key={hotel.id} {...hotel} theme={this.props.theme} />
-        ))}
-      </div>
-    );
-  }
+const slowFunction = (count) => {
+    for(let i=0; i<1200000000; i++){}
+    return count
+}
+
+function Hotels(props) {
+
+  const count = useMemo(() => {return slowFunction(props.hotels.length)}, [props.hotels.length])
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Oferty({count}):</h2>
+      {props.hotels.map((hotel) => (
+        <Hotel key={hotel.id} {...hotel} theme={props.theme} />
+      ))}
+    </div>
+  );
 }
 
 Hotels.propTypes = {
