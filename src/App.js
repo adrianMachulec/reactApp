9e6855d777565bfe2e0a9ca79d4678145137317b
Adrian Machulec <adriandamianmachulec@gmail.com>
2023-04-11 22:47:33 +0200

    useState to useReducer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect, useReducer } from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -11,94 +11,119 @@ import ThemeButton from "./components/UI/ThemeButton/ThemeButton";
 import ThemeContext from "./context/themeContext";
 import AuthContext from "./context/authContext";
 
-class App extends Component {
-  hotels = [
-    {
-      id: 1,
-      name: "Pod akacjami",
-      city: "Warszawa",
-      rating: 8.3,
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vestibulum, risus vitae venenatis pulvinar, neque velit laoreet quam, a blandit enim dolor ac ligula.",
-      image: "",
-    },
-    {
-      id: 2,
-      name: "Dębowy",
-      city: "Lublin",
-      rating: 8.8,
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vestibulum, risus vitae venenatis pulvinar, neque velit laoreet quam, a blandit enim dolor ac ligula.",
-      image: "",
-    },
-  ];
+const backendHotels = [
+  {
+    id: 1,
+    name: "Pod akacjami",
+    city: "Warszawa",
+    rating: 8.3,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vestibulum, risus vitae venenatis pulvinar, neque velit laoreet quam, a blandit enim dolor ac ligula.",
+    image: "",
+  },
+  {
+    id: 2,
+    name: "Dębowy",
+    city: "Lublin",
+    rating: 8.8,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vestibulum, risus vitae venenatis pulvinar, neque velit laoreet quam, a blandit enim dolor ac ligula.",
+    image: "",
+  },
+];
 
-  state = {
-    hotels: [],
-    loading: true,
-    theme: "danger",
-    isAuthenticated: false
-  };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "change-theme":
+      const theme = state.theme === "danger" ? "warning" : "danger"
+      return { ...state, theme };
+    case "set-hotels":
+      return {
+        ...state,
+        hotels: action.hotels,
+      };
+    case "set-loading":
+      return {
+        ...state,
+        loading: action.loading,
+      };
+    case "login":
+      return {
+        ...state,
+        isAuthenticated: true,
+      };
+    case "logout":
+      return {
+        ...state,
+        isAuthenticated: false,
+      };
+    default:
+      throw new Error("Nie ma takiej akcji: " + action.type);
+  }
+};
 
-  searchHandler(term) {
-    const hotels = [...this.hotels].filter((x) =>
+const initialState = {
+  hotels: [],
+  loading: true,
+  isAuthenticated: false,
+  theme: "danger",
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: "set-hotels", hotels: backendHotels });
+      dispatch({ type: "set-loading", loading: false });
+    }, 1000);
+  }, []);
+
+  const searchHandler = (term) => {
+    const newHotels = [...backendHotels].filter((x) =>
       x.name.toLowerCase().includes(term.toLowerCase())
     );
-    this.setState({ hotels });
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ hotels: this.hotels, loading: false });
-    }, 1000);
-  }
-
-  changeTheme = () => {
-    const newTheme = this.state.theme === "primary" ? "danger" : "primary";
-    this.setState({ theme: newTheme });
+    dispatch({ type: "set-hotels", hotels: newHotels });
   };
 
-  render() {
-    const header = (
-      <Header onSearch={(term) => this.searchHandler(term)}>
-        <Searchbar onSearch={(term) => this.searchHandler(term)} />
-        <ThemeButton />
-      </Header>
-    );
+  const header = (
+    <Header>
+      <Searchbar onSearch={(term) => searchHandler(term)} />
+      <ThemeButton />
+    </Header>
+  );
 
-    const menu = <Menu />;
+  const menu = <Menu />;
 
-    const content = this.state.loading ? (
-      <LoadingIcon />
-    ) : (
-      <Hotels hotels={this.state.hotels} />
-    );
+  const content = state.loading ? (
+    <LoadingIcon />
+  ) : (
+    <Hotels hotels={state.hotels} />
+  );
 
-    const footer = <Footer />;
+  const footer = <Footer />;
 
-    return (
-      <AuthContext.Provider 
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated: state.isAuthenticated,
+        login: () => dispatch({ type: "login" }),
+        logout: () => dispatch({ type: "logout" }),
+      }}
+    >
+      <ThemeContext.Provider
         value={{
-          isAuthenticated: this.state.isAuthenticated,
-          login: () => this.setState({isAuthenticated: true}),
-          logout: () => this.setState({isAuthenticated: false})
-        }}>
-        <ThemeContext.Provider
-          value={{
-            color: this.state.theme,
-            changeTheme: this.changeTheme,
-          }}
-        >
-          <Layout
-            header={header}
-            menu={menu}
-            content={content}
-            footer={footer}
-          />
-        </ThemeContext.Provider>
-      </AuthContext.Provider>
-    );
-  }
+          color: state.theme,
+          changeTheme: () =>
+            dispatch({
+              type: "change-theme",
+            }),
+        }}
+      >
+        <Layout header={header} menu={menu} content={content} footer={footer} />
+      </ThemeContext.Provider>
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
--- src/components/Hotels/Hotels.js
+++ src/components/Hotels/Hotels.js
@@ -1,21 +1,23 @@
-import {Component} from "react"
-import Hotel from "./Hotel/Hotel"
-import styles from './Hotels.module.css'
-import PropTypes from 'prop-types'
+import { PureComponent } from "react";
+import Hotel from "./Hotel/Hotel";
+import styles from "./Hotels.module.css";
+import PropTypes from "prop-types";
 
-class Hotels extends Component {
-    render(){
-        return(
-            <div className={styles.container}>
-                <h2 className={styles.title}>Oferty:</h2>
-                {this.props.hotels.map(hotel => <Hotel key={hotel.id} {...hotel} theme={this.props.theme} />)}
-            </div>
-        )
-    }
+class Hotels extends PureComponent {
+  render() {
+    return (
+      <div className={styles.container}>
+        <h2 className={styles.title}>Oferty:</h2>
+        {this.props.hotels.map((hotel) => (
+          <Hotel key={hotel.id} {...hotel} theme={this.props.theme} />
+        ))}
+      </div>
+    );
+  }
 }
 
 Hotels.propTypes = {
-    hotels: PropTypes.array.isRequired
-}
+  hotels: PropTypes.array.isRequired,
+};
 
-export default Hotels
+export default Hotels;
